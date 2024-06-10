4c109dc48f3d43630542b10d46a8ba1807d281b9
Wu-Lezheng <wulezheng00@gmail.com>
2024-06-11 00:18:40 +0800

    Edit company highlight images

--- src/components/company-highlight/CompanyHighlight.jsx
+++ src/components/company-highlight/CompanyHighlight.jsx
@@ -31,7 +31,7 @@ function CompanyHighlight() {
                 style={{ '--light-x': glowStates[0].x, '--light-y': glowStates[0].y }}>
                 <div className={styles.containerBorder}></div>
                 <div className={styles.featureContent}>
-                    <Image src="/innovative.png" alt='Innovative' width={140} height={140}></Image>
+                    <Image src="/bulb-dynamic-gradient.svg" alt='Innovative' width={150} height={150} style={{ marginBottom: '0.5rem' }}></Image>
                     <h2>Innovative</h2>
                     <p> ST Engineering drives technological advancement through pioneering solutions in aerospace, electronics, land systems, and marine sectors</p>
                 </div>
@@ -40,14 +40,16 @@ function CompanyHighlight() {
                 style={{ '--light-x': glowStates[1].x, '--light-y': glowStates[1].y }}>
                 <div className={styles.containerBorder}></div>
                 <div className={styles.featureContent}>
+                    <Image src="/sphere-dynamic-gradient.svg" alt='Innovative' width={150} height={150} style={{ marginBottom: '0.5rem' }}></Image>
                     <h2>Global</h2>
-                    <p>With a presence in over 100 countries, ST Engineering provides diverse, high-tech services and products on a global scale</p>
+                    <p>With a presence in over 100 countries, ST Engineering provides diverse, high-tech services and cutting-edge products on a global scale</p>
                 </div>
             </div>
             <div ref={glowContainerRefs[2]} className={styles.glowContainer}
                 style={{ '--light-x': glowStates[2].x, '--light-y': glowStates[2].y }}>
                 <div className={styles.containerBorder}></div>
                 <div className={styles.featureContent}>
+                    <Image src="/sheild-dynamic-gradient.svg" alt='Innovative' width={150} height={150} style={{ marginBottom: '0.5rem' }}></Image>
                     <h2>Reliable</h2>
                     <p>Known for its commitment to quality and safety, ST Engineering delivers dependable engineering solutions across critical industries worldwide</p>
                 </div>
