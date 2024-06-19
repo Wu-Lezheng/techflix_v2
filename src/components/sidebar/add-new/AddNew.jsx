acfdef2f124afcaab563e46d02baaf411444db45
Wu Lezheng <wulezheng00@gmail.com>
2024-06-19 14:16:51 +0800

    Prevent non-admin to make changes

--- src/components/sidebar/add-new/AddNew.jsx
+++ src/components/sidebar/add-new/AddNew.jsx
@@ -1,8 +1,15 @@
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { BsPlusLg } from "react-icons/bs";
 import styles from "./AddNew.module.css";
 
 export default function AddNew() {
+
+    const { data: session, status } = useSession();
+    if (session?.user.isAdmin === false) {
+        return;
+    }
+
     return (
         <div className={styles.container}>
             <Link href="?add-new=true" scroll={false}>
